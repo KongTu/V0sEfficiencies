42297d929431e15d39aa4137c662b682505b16d9
Kong <tuzhoudunming@gmail.com>
2014-06-26 12:03:18 -0400

    Update jetConeEfficiency_Regit.C

--- jetConeEfficiency_Regit.C
+++ jetConeEfficiency_Regit.C
@@ -258,7 +258,7 @@ double la_YieldCal( TH1F* inputHist ){
 
 void jetConeEfficiency_Regit(){
 
-	TFile* file = new TFile("~/Desktop/Kongwork/RegitPYTHIA80_JUNE24_ppCuts_2014.root");
+    TFile* file = new TFile("~/Desktop/Kongwork/RegitPYTHIA80_JUNE24_ppCuts_2014.root");
     TTree* theTree = ( TTree* )file->Get("v0analyzerNew/PFJet"); 
     TTree* KshortTree = ( TTree* )file->Get("v0analyzerNew/v0_Kshort");
     TTree* LambdaTree = ( TTree* )file->Get("v0analyzerNew/v0_Lambda"); 
